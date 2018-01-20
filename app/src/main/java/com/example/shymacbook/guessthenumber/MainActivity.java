6e5606d3e6b484bd8295c15b9862d02870db8781
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-01-20 15:58:34 -0800

    submit function broken

--- app/src/main/java/com/example/shymacbook/guessthenumber/MainActivity.java
+++ app/src/main/java/com/example/shymacbook/guessthenumber/MainActivity.java
@@ -5,9 +5,6 @@
 //  Description:    cs470 hw 01
 //                      Android app - number guessing game
 //
-//      Problem:    fail to get the edit text value
-//                  and compare it with winning value
-//
 //*********************************************************
 
 
@@ -25,48 +22,66 @@ import java.util.Random;
 
 import static com.example.shymacbook.guessthenumber.R.id.GuessHere;
 
+
+
 public class MainActivity extends AppCompatActivity {
 
+
     public static int rand1;
+    public static int guessINT;
     public static int myGuessINT;
+    Random randy = new Random();
+    EditText guessINPUT = findViewById(R.id.GuessHere);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // set the MY GUESS value from the edit text box
-//        String value= GuessHere.getText().toString();
-//        String value= (findViewById(R.id.GuessHere)).getContext().getText(');
-//        myGuessINT=Integer.parseInt(value);
-        EditText text = (EditText)findViewById(R.id.GuessHere);
-        String value = text.getText().toString();
-        myGuessINT = Integer.parseInt(value);
-
-        // random number function
-        Random randy = new Random();
-        rand1 = randy.nextInt(500);
+        // initialize the random number
+//        rand1 = randy.nextInt(500);
+        roll();
     }
+
+
 
     // make button actions
     // HINT button
     public void hint_butt_click(View view) {
         // implementation for HINT button action
-        Toast.makeText(this,"Number = " + rand1,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Number = " + rand1, Toast.LENGTH_SHORT).show();
     }
+
+
 
     // SUBMIT button
     public void submit_butt_click(View view) {
-        if(myGuessINT == rand1){
-            Toast.makeText(this,"GOOD GUESS!",Toast.LENGTH_SHORT).show();
-            Random randy2 = new Random();
-            rand1 = randy2.nextInt(500);
+        // take the guess input value from edit text field, and convert to integer
+        String guessSTRING = guessINPUT.getText().toString();
+        guessINT = Integer.parseInt(guessSTRING);
+        if (guessINT == rand1){
+            Toast.makeText(this, "CORRECT!", Toast.LENGTH_SHORT).show();
+//            rand1 = randy.nextInt(500);
+            roll();
         }
-        else if (myGuessINT < rand1){
-            Toast.makeText(this,"too low, guess higher...",Toast.LENGTH_SHORT).show();
+        else if (guessINT < rand1){
+            Toast.makeText(this, "too low, guess higher...", Toast.LENGTH_SHORT).show();
         }
-        else if (myGuessINT > rand1){
-            Toast.makeText(this,"too high, guess lower...",Toast.LENGTH_SHORT).show();
+        else if (guessINT > rand1){
+            Toast.makeText(this, "too high, guess lower...", Toast.LENGTH_SHORT).show();
         }
     }
+
+
+    // roll function
+    public void roll(){
+        rand1 = randy.nextInt(500);
+        System.out.println("rand1 = " + rand1);
+    }
+
+
+
+
 }
